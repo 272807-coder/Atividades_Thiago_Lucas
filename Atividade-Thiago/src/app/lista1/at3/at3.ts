c44fa96c37047a4d05ffda1c6814ced3e9b76c54
Lucas_hbm <272807@unifio.edu.br>
2026-09-22 02:17:56 +0000

    criação de Modulos e Componentes

--- Atividade-Thiago/src/app/lista1/at3/at3.ts
+++ Atividade-Thiago/src/app/lista1/at3/at3.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./at3.css']
 })
 export class At3Component {
-  imagemProduto = 'https://placehold.co/200';
+  imagemProduto = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMcUXRZl8-RkeE2QQj9C1N2LXrhpOvkOdzJbNMQnGoLA&s=10';
   descricaoImagem = 'Imagem do produto';
 }
